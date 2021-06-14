c186e0406d172b160b2dee7cecf88adb497ce169
daniel sena <danielsena04@gmail.com>
2021-06-14 16:44:28 -0300

    feat: add cli parameter to informe the website to be parsed

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/danielAang/html_tree
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
--- main.go
+++ main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/danielAang/html_tree/link"
+)
+
+func main() {
+	site := flag.String("site", "https://www.google.com", "Website to parse all links")
+	flag.Parse()
+	resp, err := http.Get(*site)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		r := strings.NewReader(string(body))
+		links, err := link.Parse(r)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", links)
+	} else {
+		fmt.Println("Unable to get site:", site)
+	}
+}
